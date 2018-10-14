7bea643a774f5b04407f075bc4be54e8072405df
muhammadmateen02 <muhammadmateen02@gmail.com>
2018-10-14 21:47:55 +0500

    create design of resturant branches

--- src/app/branch-info/branches-list/branches-list.component.ts
+++ src/app/branch-info/branches-list/branches-list.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-branches-list',
@@ -7,9 +19,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BranchesListComponent implements OnInit {
 
-  constructor() { }
+  public model: any;
+  public dateModel: any;
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+
+  public fabBtnOptions = {
+    position: 'top',
+    value: 'Add'
+  }
+  public page = 1;
+
+  pageChange(e){
+    console.log("Page Change : ",e);
+  }
+
+  search = (text$: Observable<string>) =>
+      text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        map(term => states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+      )
+
+  openDialog(modalRef, type:string){
+    // if(type == 'newBranch'){
+
+    // }
+
+    this.modalService.open(modalRef, { size: 'lg', ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log("Result : ",result);
+      //this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      console.log("Reason : ",reason);
+      //this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+
   }
 
 }
