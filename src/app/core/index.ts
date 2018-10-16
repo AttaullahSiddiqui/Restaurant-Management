ac3897c8bf9f0e57a59b980f7c54e6a4fc43f020
muhammadmateen <muhammadmateen02@gmail.com>
2018-10-16 17:47:25 +0500

    Move core-components to core folder

---
+++ src/app/core/index.ts
@@ -0,0 +1,10 @@
+/* Components */
+export * from './components/admin-layout/admin-layout.component';
+export * from './components/auth-layout/auth-layout.component';
+export * from './components/header/header.component';
+export * from './components/sidebar/sidebar.component';
+export * from './components/not-found404/not-found404.component';
+
+/* Services */
+export * from './services/http.service';
+export * from './services/utility.service';
