a0e767d7c4de304db4588a09d6ef1d419f3d6b0a
muhammadmateen <muhammadmateen02@gmail.com>
2018-12-29 19:01:23 +0500

    create user list and user request components

---
+++ src/app/featured-modules/private/users/user-request/user-request.component.ts
@@ -0,0 +1,213 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-request',
+  templateUrl: './user-request.component.html',
+  styleUrls: ['./user-request.component.scss']
+})
+export class UserRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  dummyData = [{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },
+  {
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  },{
+    userName : 'muhammadmateen02',
+    mobileNo: '03222642342',
+    role: '1',
+    status: 'Pending'
+  }]
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import {Observable, Subscription} from 'rxjs';
+// import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+// import { HttpService, AppToastrService } from '@app/core';
+// declare var $:any;
+
+// @Component({
+//   selector: 'app-create-bills',
+//   templateUrl: './create-bills.component.html',
+//   styleUrls: ['./create-bills.component.scss']
+// })
+// export class CreateBillsComponent implements OnInit, OnDestroy {
+//   @ViewChild('quantity') quantityInput : ElementRef;
+
+//   requestPending : boolean = false;
+//   restaurantMenu : menuDetail[] = [];
+//   billItem : billItem[] = [];
+//   billForm : FormGroup;
+//   isFormSubmit: boolean = false;
+//   private formSubscriber : FormSubscriber = {};
+//   itemCost : any = '- -';
+//   totalItemPrice : any = '- -';
+
+//   constructor(
+//     public http : HttpService,
+//     public _toastrService : AppToastrService,
+//     private fb: FormBuilder
+//   ) { }
+
+//   ngOnInit() {
+//     this.getMenu();
+//     this.createBillForm();
+//     this.subscribeFormChanges();
+//   }
+
+//   ngOnDestroy(){
+//     this.unSubscribeFormChanges();
+//   }
+
+
+//   createBillForm(){
+//     this.billForm = this.fb.group({
+//       itemDetail    : ['', Validators.required],
+//       itemQuantity  : ['', Validators.required],
+//     })
+//   };
+
+//   subscribeFormChanges(){
+
+//     this.formSubscriber.detail = this.billForm.get('itemDetail').valueChanges.pipe(
+//         distinctUntilChanged()
+//       ).subscribe((data) => {
+//         data = JSON.parse(data);
+//         if(data){
+//           this.itemCost = data.price +'/'+data.unit;
+//         }
+//         this.quantityInput.nativeElement.focus();
+//         console.log("Item Details change : ",data.price);
+//     }) 
+
+//     this.formSubscriber.quantity = this.billForm.get('itemQuantity').valueChanges.pipe(
+//        distinctUntilChanged()
+//       ).subscribe((data) => {
+//         if(data && this.itemCost != '- -' ){
+//           this.totalItemPrice = +this.itemCost * +data.price;
+//         }
+//         console.log("Item Quantity change : ",data);
+//     })   
+//   }
+
+//   calculateAmount(){
+
+//   }
+
+//   unSubscribeFormChanges(){
+//     this.formSubscriber.detail.unsubscribe();
+//     this.formSubscriber.quantity.unsubscribe();
+//     console.log("unsubscribe call : ",this.formSubscriber);
+//   }
+
+//   getMenu(){
+//     this.requestPending = true;
+//     this.http.get('menu').subscribe(result => {
+//         console.log("Menu Fetch : ",result);
+//         this.requestPending = false;
+//         this.restaurantMenu = result.body.data;
+//         setTimeout(() => {
+//           $('.selectpicker').selectpicker('refresh');
+//         }, 0);
+//     }, err => {
+//         this.requestPending = false;
+//         console.log("Error : ",err);
+//     });
+//   };
+
+//   addItem(valid, data){
+//     this.isFormSubmit = true;
+//     if(!valid){
+//       return ;
+//     }
+//     console.log("Data :", JSON.parse(data.itemDetail)); 
+//   }
+
+//   resetForm(){
+//     this.billForm.reset();
+//     this.isFormSubmit = false;
+//   }
+
+// }
+
+// interface billItem {
+//   itemId : string,
+//   itemName : string
+//   cost :Number
+//   quantity : Number 
+//   price : Number
+// }
+
+// interface menuDetail {
+//   _id: string;
+//   categoryName: string,
+//   city: string,
+//   categoryItems: menuItemDetail[],
+//   branchAddress: string
+// }
+
+// interface menuItemDetail {
+//   itemName : string
+//   price :Number
+//   quantity : Number 
+//   unit : string
+// }
+
+// interface FormSubscriber{
+//   detail? : Subscription,
+//   quantity? : Subscription
+// }
--- src/app/featured-modules/private/users/users.module.ts
+++ src/app/featured-modules/private/users/users.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserComponent } from './user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserRequestComponent } from './user-request/user-request.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule,
     UsersRoutingModule
   ],
   declarations: [
-    UserComponent
+    UserComponent,
+    UserListComponent,
+    UserRequestComponent
   ]
 })
 export class UsersModule { }
