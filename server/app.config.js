5978d588987c0d24408626ce8ff6048b05a7cc97
muhammadmateen <muhammadmateen02@gmail.com>
2019-01-10 18:37:57 +0500

    add twilio config

--- server/app.config.js
+++ server/app.config.js
@@ -6,6 +6,11 @@ const config = {
     },
     JWT : {
         secretKey : 'hfgs97454089jbjsdfsdvf87032603jb'
+    },
+    twilio: {
+        accountSID : 'ACf52b98a28838a65a827eca710f56b079',
+        authToken : 'b1cd052b21d966b0671f16ab325b0ccb',
+        twilioPhoneNo: '+18632702045'
     }
 }
 
